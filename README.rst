f896cd6e8023a2815b5d7243cb6781a371757aa2
Varchas Gopalaswam <vgop@lle.rochester.edu>
2018-05-29 16:15:38 -0400

    Modified setup.py

---
+++ README.rst
@@ -0,0 +1,4 @@
+# NMMSO
+
+Python implementation of the Niching Migratory Multi-swarm Optimiser (https://ieeexplore.ieee.org/document/6900309/) by Fieldsend.
+
--- setup.py
+++ setup.py
@@ -6,5 +6,13 @@ setup(name='NMMSO',
       author='Varchas Gopalaswamy',
       author_email='vgop@lle.rochester.edu',
       license='MIT',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
       packages=find_packages(),
+      download_url = 'https://github.com/varchasgopalaswamy/NMMSO/archive/1.0.tar.gz',
+      keywords = ['optimizer','black','box','black-box','optimize','swarm','particle','niching','multi-modal'],
+      install_requires=['numpy'],
       zip_safe=False)
